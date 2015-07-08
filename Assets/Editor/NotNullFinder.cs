5f11e3f13cecda5e5589a64d7b7403b65a83a33b
Edward Rowe <edward.l.rowe@gmail.com>
2015-07-08 13:10:32 -0400

    Split out ErrorGameObject into new class, NotNullError.
    This will be used in a test script.

--- Assets/Editor/NotNullFinder.cs
+++ Assets/Editor/NotNullFinder.cs
@@ -43,18 +43,18 @@ namespace RedBlueTools
 
 		static void ErrorForNullRequiredWiresOnGameObject (GameObject gameObject, string pathToAsset)
 		{
-			List<ErrorGameObject> erroringObjects = new List<ErrorGameObject> ();
+			List<NotNullError> erroringObjects = new List<NotNullError> ();
 			TraverseGameObjectHierarchyForErrors (gameObject, pathToAsset, ref erroringObjects);
-			foreach (ErrorGameObject errorObject in erroringObjects) {
+			foreach (NotNullError errorObject in erroringObjects) {
 				errorObject.OutputError ();
 			}
 			return;
 		}
 
-		static void TraverseGameObjectHierarchyForErrors (GameObject obj, string assetPath, ref List<ErrorGameObject> errorings)
+		static void TraverseGameObjectHierarchyForErrors (GameObject obj, string assetPath, ref List<NotNullError> errorings)
 		{ 
-			if (ErrorGameObject.ObjectHasErrors (obj)) {
-				errorings.Add (new ErrorGameObject (obj, assetPath));
+			if (NotNullError.ObjectHasErrors (obj)) {
+				errorings.Add (new NotNullError (obj, assetPath));
 			}
 			foreach (Transform child in obj.transform) {
 				TraverseGameObjectHierarchyForErrors (child.gameObject, assetPath, ref errorings);
@@ -67,135 +67,6 @@ namespace RedBlueTools
 				return;
 			}
 			Debug.Log (log);
-		}
-
-		class ErrorGameObject
-		{
-			public GameObject errorObject;
-			public List<ErrorMonoBehaviour> errorMonoBehaviours;
-			public string AssetPath;
-			public string FullName
-			{
-				get {
-					Transform currentParent = errorObject.transform.parent;
-					string fullName = errorObject.name;
-					while (currentParent != null) {
-						fullName = currentParent.gameObject.name + "/" + fullName;
-						currentParent = currentParent.transform.parent;
-					}
-					return fullName;
-				}
-			}
-
-			public int NumErroringMonoBehaviours {
-				get {
-					if (errorMonoBehaviours == null) {
-						return 0;
-					} else {
-						return errorMonoBehaviours.Count;
-					}
-				}
-			}
-
-			public ErrorGameObject (GameObject sourceObject, string assetPath)
-			{
-				this.errorObject = sourceObject;
-				this.AssetPath = assetPath;
-
-				MonoBehaviour[] monobehaviours = errorObject.GetComponents<MonoBehaviour> ();
-				this.errorMonoBehaviours = new List<ErrorMonoBehaviour> ();
-				for (int i = 0; i < monobehaviours.Length; i++) {
-					ErrorMonoBehaviour errors = new ErrorMonoBehaviour (monobehaviours [i]);
-					if (errors.NumErrorFields > 0) {
-						this.errorMonoBehaviours.Add (errors);
-					}
-				}
-			}
-
-			public static bool ObjectHasErrors (GameObject gameObject)
-			{
-				MonoBehaviour[] monobehaviours = gameObject.GetComponents<MonoBehaviour> ();
-				for (int i = 0; i < monobehaviours.Length; i++) {
-					if (ErrorMonoBehaviour.MonoBehaviourHasErrors ( monobehaviours[i])) {
-						return true;
-					}
-				}
-
-				return false;
-			}
-
-			public void OutputError ()
-			{
-				foreach (ErrorMonoBehaviour errorMB in errorMonoBehaviours) {
-					foreach( ErrorField error in errorMB.errors) {
-						Debug.LogError (string.Format ("NotNull field: {0} " +
-						                               "has not been assigned on object: {1}\nPath: {2}",
-						                               error.fieldInfo.Name, FullName, AssetPath), this.errorObject);
-					}
-				}
-			}
-		}
-
-		class ErrorMonoBehaviour
-		{
-			public MonoBehaviour sourceBehaviour;
-			public bool IsMissing;
-			public List<ErrorField> errors;
-
-			public int NumErrorFields {
-				get {
-					if (errors == null) {
-						return 0;
-					} else {
-						return errors.Count;
-					}
-				}
-			}
-
-			public ErrorMonoBehaviour (MonoBehaviour sourceMB)
-			{
-				this.sourceBehaviour = sourceMB;
-				this.IsMissing = sourceMB == null;
-
-				if (sourceMB != null) {
-					this.errors = new List<ErrorField> ();
-					List<FieldInfo> erroringFields = GetErroringFields (sourceMB);
-					foreach (FieldInfo erroringField in erroringFields) {
-						this.errors.Add (new ErrorField (erroringField, sourceMB));
-					}
-				}
-			}
-
-			static List<FieldInfo> GetErroringFields (MonoBehaviour sourceMB)
-			{
-				List<FieldInfo> erroringFields = new List<FieldInfo> ();
-				List<FieldInfo> notNullFields = ReflectionUtilities.GetMonoBehaviourFieldsWithAttribute<NotNullAttribute> (sourceMB);
-				foreach (FieldInfo notNullField in notNullFields) {
-					object fieldObject = notNullField.GetValue (sourceMB);
-					if (fieldObject == null || fieldObject.Equals (null)) {
-						erroringFields.Add (notNullField);
-					}
-				}
-
-				return erroringFields;
-			}
-
-			public static bool MonoBehaviourHasErrors (MonoBehaviour mb)
-			{
-				return GetErroringFields (mb).Count > 0;
-			}
-		}
-
-		class ErrorField
-		{
-			public FieldInfo fieldInfo;
-			public MonoBehaviour sourceBehaviour;
-
-			public ErrorField (FieldInfo fieldInfo, MonoBehaviour sourceMB)
-			{
-				this.fieldInfo = fieldInfo;
-				this.sourceBehaviour = sourceMB;
-			}
 		}
 	}
 }
---
+++ Assets/Scripts/NotNullError.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace RedBlueTools
+{
+	public class NotNullError
+	{
+		public GameObject ErrorGameObject;
+		public List<ErrorMonoBehaviour> MonoBehavioursWithErrors;
+		public string AssetPath;
+
+		public string FullName {
+			get {
+				Transform currentParent = ErrorGameObject.transform.parent;
+				string fullName = ErrorGameObject.name;
+				while (currentParent != null) {
+					fullName = currentParent.gameObject.name + "/" + fullName;
+					currentParent = currentParent.transform.parent;
+				}
+				return fullName;
+			}
+		}
+	
+		public int NumErroringMonoBehaviours {
+			get {
+				if (MonoBehavioursWithErrors == null) {
+					return 0;
+				} else {
+					return MonoBehavioursWithErrors.Count;
+				}
+			}
+		}
+	
+		public NotNullError (GameObject sourceObject, string assetPath)
+		{
+			this.ErrorGameObject = sourceObject;
+			this.AssetPath = assetPath;
+		
+			MonoBehaviour[] monobehaviours = ErrorGameObject.GetComponents<MonoBehaviour> ();
+			this.MonoBehavioursWithErrors = new List<ErrorMonoBehaviour> ();
+			for (int i = 0; i < monobehaviours.Length; i++) {
+				if (ErrorMonoBehaviour.MonoBehaviourHasErrors (monobehaviours [i])) {
+					this.MonoBehavioursWithErrors.Add (new ErrorMonoBehaviour (monobehaviours [i]));
+				}
+			}
+		}
+	
+		public static bool ObjectHasErrors (GameObject gameObject)
+		{
+			MonoBehaviour[] monobehaviours = gameObject.GetComponents<MonoBehaviour> ();
+			for (int i = 0; i < monobehaviours.Length; i++) {
+				if (ErrorMonoBehaviour.MonoBehaviourHasErrors (monobehaviours [i])) {
+					return true;
+				}
+			}
+		
+			return false;
+		}
+	
+		public void OutputError ()
+		{
+			foreach (ErrorMonoBehaviour errorMB in MonoBehavioursWithErrors) {
+				foreach (ErrorField error in errorMB.ErrorFields) {
+					Debug.LogError (string.Format ("NotNull field: {0} " +
+						"has not been assigned on object: {1}\nPath: {2}",
+				        error.fieldInfo.Name, FullName, AssetPath), this.ErrorGameObject);
+				}
+			}
+		}
+	}
+
+	public class ErrorMonoBehaviour
+	{
+		public MonoBehaviour SourceMonoBehaviour;
+		public bool IsMissing;
+		public List<ErrorField> ErrorFields;
+		
+		public int NumErrorFields {
+			get {
+				if (ErrorFields == null) {
+					return 0;
+				} else {
+					return ErrorFields.Count;
+				}
+			}
+		}
+		
+		public ErrorMonoBehaviour (MonoBehaviour sourceMB)
+		{
+			this.SourceMonoBehaviour = sourceMB;
+			this.IsMissing = sourceMB == null;
+			
+			if (sourceMB != null) {
+				this.ErrorFields = new List<ErrorField> ();
+				List<FieldInfo> erroringFields = GetErroringFields (sourceMB);
+				foreach (FieldInfo erroringField in erroringFields) {
+					this.ErrorFields.Add (new ErrorField (erroringField, sourceMB));
+				}
+			}
+		}
+		
+		static List<FieldInfo> GetErroringFields (MonoBehaviour sourceMB)
+		{
+			List<FieldInfo> erroringFields = new List<FieldInfo> ();
+			List<FieldInfo> notNullFields = ReflectionUtilities.GetMonoBehaviourFieldsWithAttribute<NotNullAttribute> (sourceMB);
+			foreach (FieldInfo notNullField in notNullFields) {
+				object fieldObject = notNullField.GetValue (sourceMB);
+				if (fieldObject == null || fieldObject.Equals (null)) {
+					erroringFields.Add (notNullField);
+				}
+			}
+			
+			return erroringFields;
+		}
+		
+		public static bool MonoBehaviourHasErrors (MonoBehaviour mb)
+		{
+			return GetErroringFields (mb).Count > 0;
+		}
+	}
+	
+	public class ErrorField
+	{
+		public FieldInfo fieldInfo;
+		public MonoBehaviour sourceBehaviour;
+		
+		public ErrorField (FieldInfo fieldInfo, MonoBehaviour sourceMB)
+		{
+			this.fieldInfo = fieldInfo;
+			this.sourceBehaviour = sourceMB;
+		}
+	}
+}
+
