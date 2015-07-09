35c5bb9c2d393366e043f83d69d9e5b6fe118350
Edward Rowe <edward.l.rowe@gmail.com>
2015-07-09 15:17:52 -0400

    Organized project for packaging NotNullAttribute.

---
+++ Assets/NotNullAttribute/Editor/FindNotNullsOnStart.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[InitializeOnLoad]
+public class FindNotNullsOnStart {
+	
+	static FindNotNullsOnStart ()
+	{
+		if(Debug.isDebugBuild) {
+			RedBlueTools.NotNullFinder.SearchForAndErrorForNotNullViolations ();
+		}
+	}
+}
